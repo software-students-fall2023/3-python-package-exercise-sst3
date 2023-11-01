63dda9c43f7c1323ac12a9a36fc7d05377816413
Silver1793 <git>
2023-11-01 11:28:16 -0400

    made some changes

--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -1,10 +1,14 @@
 import pytest
 from src.sstgame import madlib
-class Tests:
 
-    @pytest.fixture
-    def test_first(self):
-        assert 3 == 3
-    # def test_ReadTextSize():
-    #     prompts = readText("text.txt")
-    #     assert(len(prompts) == 3)
+def test_first():
+    assert 3 == 3
+
+# class Tests:
+
+#     @pytest.fixture
+#     def test_first(self):
+#         assert 3 == 3
+#     # def test_ReadTextSize():
+#     #     prompts = readText("text.txt")
+#     #     assert(len(prompts) == 3)
