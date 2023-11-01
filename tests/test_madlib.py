ef3080b7c795badb83abaaab819a6da0b5f18412
Silver1793 <git>
2023-11-01 13:49:04 -0400

    feat: created function and tests to find words in []

--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -4,11 +4,18 @@ from src.sstgame import madlib
 def test_first():
     assert 3 == 3
 
-# class Tests:
+def test_extractWordsEmpty():
+    matches = madlib.extractWords("This is something that has no extraction")
+    assert len(matches) == 0
 
-#     @pytest.fixture
-#     def test_first(self):
-#         assert 3 == 3
-#     # def test_ReadTextSize():
-#     #     prompts = readText("text.txt")
-#     #     assert(len(prompts) == 3)
+def test_extractWordsOne():
+    matches = madlib.extractWords("There should be one extraction here [verb]")
+    assert len(matches) == 1
+    assert matches[0] == "verb"
+
+def test_extractWordsMultiple():
+    matches = madlib.extractWords("There should be [multiple] [extractions] here [verb]")
+    assert len(matches) == 3
+    assert matches[0] == "multiple"
+    assert matches[1] == "extractions"
+    assert matches[2] == "verb"
