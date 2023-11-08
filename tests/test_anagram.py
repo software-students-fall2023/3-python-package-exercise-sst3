fe6d0f59a047307fda3f8f590680b3ccf3d29c79
Ivan Jing <ij2036@nyu.edu>
2023-11-07 23:47:01 -0500

    Update test_anagram.py

--- tests/test_anagram.py
+++ tests/test_anagram.py
@@ -1,23 +1,16 @@
 import pytest
-import os
+import tempfile
 from src.sstgame.anagram import makeSet, generateLetters, is_valid_answer, calcScore
 from io import StringIO
 
-@pytest.fixture
-def words_set():
-
-    fileName = '/home/runner/work/3-python-package-exercise-sst3/3-python-package-exercise-sst3/src/sstgame/anagram.txt'
-    return makeSet(fileName)
-
+    
 def test_len_generateLetters():
     letters = generateLetters(7)
-
     assert len(letters)==7 #Check if the result of generateLetters is the proper length
     assert any(letter in 'aeiou' for letter in letters) #Check if there is a vowel in letters
-     
-    
 
-def test_is_valid_answer(words_set):
+def test_is_valid_answer():
+    words_set= {'pear', 'ear','apple'}
     letters = ['p','e','a','r']
 
     assert is_valid_answer(words_set, 'pear', letters) == True #should work
