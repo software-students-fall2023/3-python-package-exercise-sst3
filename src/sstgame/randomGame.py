aa403451dcdeec7966443506ea15d2f361200ed5
Silver1793 <git>
2023-11-08 02:12:57 -0500

    test changes

--- src/sstgame/randomGame.py
+++ src/sstgame/randomGame.py
@@ -1,8 +1,8 @@
-from src.sstgame import numberGuess
-from src.sstgame import madlib
-from src.sstgame import wyr
-from src.sstgame import anagram
-from src.sstgame import hangman
+from sstgame import numberGuess
+from sstgame import madlib
+from sstgame import wyr
+from sstgame import anagram
+from sstgame import hangman
 import random
 
 def randomGame(
